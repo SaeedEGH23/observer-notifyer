a5398c186f066a1613a07bf21c0ebf2702b9c68f
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-09 22:35:12 +0330

    Add logger to index for logging notification status

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const sendNotify = require("./src/notify");
+const logger = require("./src/logger");
 const servers = ["127.0.0.1"];
 const port = 3001;
 const filePath =
@@ -13,8 +14,14 @@ const notifyServers = (changes) => {
     // (assuming a function called "sendChanges" exists to handle this)
 
     sendNotify(server, "changes", port)
-      .then(() => console.log("Notification was sent"))
-      .catch((err) => console.log("Error sending notification", err));
+      .then(() => {
+        console.log("Notification sent");
+        logger.log(server, " Notification sent");
+      })
+      .catch((err) => {
+        console.log("Error sending notification", err);
+        logger.log(server, " Error sending notification");
+      });
 
     console.log(++bb, server);
   });
